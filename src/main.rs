cf5ca204d84bbbebc7792f325470214480c923a4
Alexey <gr4k@bk.ru>
2025-01-29 20:44:43 +0300

    migrate to teloxide

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,21 @@
-extern crate dotenv;
-
-use dotenv::dotenv;
 use std::env;
+use dotenv::dotenv;
+use teloxide::prelude::*;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     dotenv().ok();
-    let TELEGRAM_BOT_TOKEN: String = env::var("TELEGRAM_BOT_TOKEN").unwrap();
-    println!("The bot token is {TELEGRAM_BOT_TOKEN}");
+    let tg_bot_token: String = env::var("TELEGRAM_BOT_TOKEN").unwrap();
+    println!("The bot token is {tg_bot_token}");
+
+    pretty_env_logger::init();
+    log::info!("Starting throw dice bot...");
+
+    let bot = Bot::new(tg_bot_token);
+
+    teloxide::repl(bot, |bot: Bot, msg: Message| async move {
+        bot.send_dice(msg.chat.id).await?;
+        Ok(())
+    })
+    .await;
 }
