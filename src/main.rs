b12c732d03e83950952b03602f55c30236c2b3bd
Alexey <gr4k@bk.ru>
2025-02-03 22:01:36 +0300

    add compute_idf func

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,24 @@ use teloxide::prelude::*;
 mod db_operations;
 use db_operations::{try_to_create_db_tables, try_to_insert_user_data};
 mod search;
-use search::tf;
+use search::{compute_tf, compute_idf} ;
 
 #[tokio::main]
 async fn main() {
     let req_word: &str = "this";
     let words: [&str; 5] = ["this  ", "is", "a  ", "a  ", "sample"];
-    // let words: [&str; 7] = ["this", "is", "another", "another", "example", "example", "example"];
-    let term_freq: f32 = tf(req_word, &words);
-    println!("term_freq is {term_freq}");
+    let words2: [&str; 7] = ["this", "is", "another", "another", "example", "example", "example"];
+    let docs: [&[&str]; 2] = [&words, &words2];
+
+    let term_freq: f32 = compute_tf(req_word, &words);
+    println!("1 term_freq is {term_freq}");
+
+    let term_freq: f32 = compute_tf(req_word, &words2);
+    println!("2 term_freq is {term_freq}");
+
+    let term: &str = "example";
+    let idf: f32 = compute_idf(term, &docs);
+    println!("idf of {term} is {idf}");
 
     dotenv().ok();
     let tg_bot_token: String = env::var("TELEGRAM_BOT_TOKEN").unwrap();
--- src/search.rs
+++ src/search.rs
@@ -1,9 +1,22 @@
-pub fn tf(req_word: &str, words: &[&str]) -> f32 {
-    let mut count: u32 = 0;
+pub fn compute_tf(req_word: &str, words: &[&str]) -> f32 {
+    let mut count: f32 = 0.0;
     for word in words {
         if word.trim() == req_word.trim() {
-            count += 1;
+            count += 1.0;
         }
     }
-    count as f32 / words.len() as f32
+    count / words.len() as f32
+}
+
+pub fn compute_idf(req_word: &str, documents: &[&[&str]]) -> f32 {
+    let mut count: f32 = 0.0;
+    for &document in documents {
+        for word in document {
+            if word.trim() == req_word.trim() {
+                count += 1.0;
+                break;
+            }
+        }
+    }
+    (documents.len() as f32 / count).log10()
 }
